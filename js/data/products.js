4a2d11647b1da6c851efe9afb5062cefe194269f
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-10 00:54:01 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,304 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Pivoting Soap Pump Dispenser',
+        price: 5.95,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Refillable soap dispenser for bathroom, kitchen, and more.',
+            },
+            {
+                text: 'One-handed pumping mechanism helps cut down on spread of bacteria.',
+            },
+            {
+                text: 'Rubber base prevents slippage.',
+            },
+            {
+                text: 'Frosted bottom allows you to see when soap is running out.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Foldable Cloth Organizer with Handles',
+        price: 19.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Set of 6 cloth storage cubes for organizing and reducing home or office clutter.',
+            },
+            {
+                text: 'Made of stylish, sturdy, breathable fabric with sewn-in, easy-to-grab handles.',
+            },
+            {
+                text: 'Function as open-top bins or as drawers when used with a storage-cube organizer (not included).',
+            },
+            {
+                text: 'Lightweight and easy to carry; collapsible for compact storage; Grey color.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Tree Trimmer',
+        price: 24.99,
+        discount: 14.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Tree trimmer provides extended reach and leveraging power for chopping through branches with ease.',
+            },
+            {
+                text: 'Sharp blade made of carbon steel with a nonstick coating for smooth cuts and long-lasting strength.',
+            },
+            {
+                text: 'Lightweight yet durable aluminum tube handles with shock-absorbing bumpers.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.jpg'],
+    },
+    {
+        id: 'product-4',
+        name: 'Portable Adapter',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img4.png',
+        description: [
+            {
+                text: '100W Charging Power: 100-watt (20V 5A) power-delivery charging. The adapter uses 8 watts of power to function; the remaining power (up to 92 watts) gets passed along to charge for the host.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png' , './img/img4-3.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Neck and Shoulder Massager',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Rotating nodes simulate hand kneading massage, and optional heating mode and adjustable intensities offer blissful relaxation.',
+            },
+            {
+                text: 'Automatic shut-off function and overheat protection provides safety and reliability.',
+            },
+            {
+                text: 'Durable and comfortable design made with a soft polyurethane and mesh exterior offering breathability and comfort.',
+            },
+            {
+                text: 'Place the massager over your shoulders and neck and lean back into relaxation.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-3.png', './img/img5-2.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Water Filter',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img6.png',
+        description: [
+            {
+                title: 'Standard Water Filter Housing',
+                text: 'This filtration component works with compatible filters to reduce sediment, chemicals, and unpleasant odors associated with unfiltered water.',
+            },
+            {
+                title: 'Easy to Install',
+                text: 'This water filter housing is simple to install and includes a 3/4 inch NPT inlet and outlet and uses either brass or CPVC fittings to fit the size of the water line.',
+            },
+            {
+                title: 'Filtered Water At Home',
+                text: 'Ideal for residential homes, this water filter housing is part of a filtration system that helps safeguard appliances and plumbing systems from sediment and scale.',
+            },
+            {
+                title: 'Peace of Mind',
+                text: 'Tested and certified against NSF/ANSI Standard 42, this filter housing is compatible with AMZN-S1A, AMZN-CW-FA, AMZN-SCWH-5, and AMZN-P5A filters.',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Enameled Cast Iron Oven',
+        price: 49.95,
+        discount: 69.99,
+        image: './img/img7.jpg',
+        description: [
+            {
+                text: 'Multifunctional laundry sorter and ironing board combination for space-saving convenience.',
+            },
+            {
+                text: 'Flat top surface made of 0.5 inch MDF board with a heat-resistant canvas cover for ironing garments and more; moves 90 degrees for easy access to laundry bags.',
+            },
+            {
+                text: '3 large-capacity, heavy-duty cloth laundry bags that can be removed from frame.',
+            },
+            {
+                text: '2 wheels with locking mechanism for easy mobility between rooms.',
+            },
+            {
+                text: 'Durable powder-coated steel frame and handles; holds up to 132 pounds.',
+            },
+        ],
+        images: ['./img/img7-1.jpg', './img/img7-2.jpg', './img/img7-3.jpg'],
+    },
+    {
+        id: 'product-8',
+        name: 'Bookcase Storage',
+        price: 69.99,
+        discount: 69.94,
+        image: './img/img8.jpg',
+        description: [
+            {
+                text: 'Non-woven Fabric.',
+            },
+            {
+                title: 'Simple stylish design',
+                text: 'Functional Cube shelf design with storage bins suitable for any room that needs a Bookcase or display shelf.',
+            },
+            {
+                text: 'Manufactured from high Quality durable composite wood and non-woven fabric. Holds up to 10 lbs per Cube.',
+            },
+            {
+                title: 'Easy assembly',
+                text: 'PLEASE follow instruction. Sturdy on even surface.',
+            },
+            {
+                title: 'Product Dimension',
+                text: '31. 6(W)x23. 6(H)x11. 9(D) inches',
+            },
+        ],
+        images: ['./img/img8-1.jpg', './img/img8-2.jpg'],
+    },
+    {
+        id: 'product-9',
+        name: 'Leather Chair',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img9.png',
+        description: [
+            {
+                text: 'Color: Black',
+            },
+            {
+                text: 'Product Dimension: 15.43" x 12.76" x 8.9"',
+            },
+            {
+                text: 'Country of Origin: China',
+            },
+            {
+                text: 'Item Weight: 10.14 lb',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png', './img/img9-4.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Electric Razor for Men',
+        price: 84.41,
+        discount: 95.50,
+        image: './img/img10.png',
+        description: [
+            {
+                title: '4-BLADE SHAVING SYSTEM FOR A PRECISE SHAVE',
+                text: 'Shaver with micro-thin stainless-steel foils captures, lifts, and holds whiskers while four super-sharp, Micro-fit blades cut hair cleanly at the base.',
+            },
+            {
+                title: 'SHAVE IN COMFORT WITH A FLEXIBLE, GENTLY CURVED PIVOTING HEAD',
+                text: 'Curved shaver head moves effortlessly across the skin, gliding in a smooth, uniform motion along the chin, jawline, neck, and head.',
+            },
+            {
+                title: 'EASILY CATCHES LONG HAIRS, AND TRIMS WITH PRECISION',
+                text: 'Acute 30-degree cutting edge to cut efficiently and cleanly at the root. Pop-up trimmer facilitates beard and mustache grooming.',
+            },
+            {
+                title: 'VERSATILE WET DRY SHAVER',
+                text: 'High-performance electric razor is perfect whether you need a quick, dry shave or prefer to use your favorite shaving foam or gel in the shower or over the sink.',
+            },
+            {
+                title: 'POWERFUL MOTOR FOR A FAST SHAVE',
+                text: 'High-performance linear motor shaves at speeds of 14,000 cycles per minute (cpm), delivering peak performance.',
+            },
+        ],
+        images: ['./img/img10-1.jpg', './img/img10-2.png', './img/img10-3.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Luggage Suitcase',
+        price: 89.99,
+        discount: 109.91,
+        image: './img/img11.png',
+        description: [
+            {
+                text: 'Interior capacity: 32 Liters',
+            },
+            {
+                text: 'Weight: 6.42 lbs.',
+            },
+            {
+                text: '22 inch rolling luggage case with durable hybrid exterior',
+            },
+            {
+                text: 'Four 360-degree spinner wheels for smooth, agile mobility',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Wireless Earphones',
+        price: 109.00,
+        discount: 110.89,
+        image: './img/img12.png',
+        description: [
+            {
+                text: 'Noise cancelling earbuds – Engineered with the world’s most effective noise cancelling. They allow you to eliminate distractions, let your surroundings in, or a little bit of each.',
+            },
+            {
+                text: 'High-fidelity audio – An exclusive acoustic architecture brings your music, podcasts, and videos to life, while Volume-optimized Active EQ keeps the sound crisp and balanced at any level.',
+            },
+            {
+                text: 'Comfortable wireless earbuds – Three sizes of  eartips are included for a secure fit. Crafted from soft silicone, they provide passive noise blocking with total comfort.',
+            },
+            {
+                text: 'Simple touch controls – It’s as easy as tap, swipe, or press. Control your noise cancellation settings, pause, play, and volume right from the side of the earbud itself. ',
+            },
+            {
+                text: 'Noise-rejecting microphones – Tiny microphones work together to focus on your voice, filtering out surrounding noises for fewer distractions during phone calls.',
+            },
+            {
+                text: 'Long battery life — Listen for up to 6 hours on a single charge. The charging case provides an additional 12 hours and charges via the included USB-C cable or wirelessly with any Qi-certified charging mat (not included).',
+            },
+            {
+                text: 'Weather & sweat resistant – These IPX4-rated wireless earbuds can withstand sweat, water, and some inclement weather.',
+            },
+        ],
+        images: ['./img/img12-1.png', './img/img12-2.png'],
+    },
+    {
+        id: 'product-13',
+        name: 'Smart watch',
+        price: 119.00,
+        discount: 120.88,
+        image: './img/img13.png',
+        description: [
+            {
+                text: 'Personalize your watch with thousands of free watch faces, apps and widgets',
+            },
+            {
+                text: 'Monitor your fitness level with VO2 max and fitness made estimates, plus keep an eye on how you handle stress',
+            },
+            {
+                text: 'Get connected features such as smart notifications and automatic uploads',
+            },
+            {
+                text: 'Battery life: Up to 7 days in smartwatch mode; 13 hours in GPS mode. Display size: 1.2 inch diameter',
+            },
+        ],
+        images: ['./img/img13-1.png', './img/img13-2.png'],
+    },
+]
